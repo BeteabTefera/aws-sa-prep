82a5ac3ad6e240bd182786a67a03d39964827f6a
Beteab Tefera <beteabtefera@yahoo.com>
2026-08-13 23:24:41 -0500

    Restructure: move terraform files into infra/

---
+++ grocerylist-resilient-aws/infra/ec2.tf
@@ -0,0 +1,60 @@
+resource "aws_security_group" "web" {
+  name_prefix = "week1-web-"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from my IP only"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"] # replace with your actual IP
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "week1-web-sg" }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.web.id]
+  key_name               = var.key_name
+
+  tags = { Name = "week1-web-instance" }
+}
+
+resource "aws_ebs_volume" "data" {
+  availability_zone = aws_instance.web.availability_zone
+  size              = 8
+  tags = { Name = "week1-data-volume" }
+}
+
+resource "aws_volume_attachment" "data_attach" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.data.id
+  instance_id = aws_instance.web.id
+}
